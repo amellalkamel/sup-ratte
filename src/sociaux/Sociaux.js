1bf4ba701b4d65f37a6772ba31badab4cc44b958
amellal.kameleddine <amellalkameleddine@gmail.com>
2020-12-31 14:19:46 +0100

    add icons to sociaux div

--- src/sociaux/Sociaux.js
+++ src/sociaux/Sociaux.js
@@ -1,11 +1,52 @@
 import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPlusSquare } from "@fortawesome/free-solid-svg-icons";
 import "./sociaux.css";
+
 class Sociaux extends React.Component {
   constructor(props) {
     super(props);
   }
   render() {
-    return <div className="sociaux"></div>;
+    return (
+      <div className="sociaux">
+        <div class="cercle">
+          {" "}
+          <FontAwesomeIcon
+            icon={faPlusSquare}
+            className="button-add-new-user-gray"
+          />
+        </div>
+        <div class="cercle">
+          {" "}
+          <FontAwesomeIcon
+            icon={faPlusSquare}
+            className="button-add-new-user-gray"
+          />
+        </div>
+        <div class="cercle">
+          {" "}
+          <FontAwesomeIcon
+            icon={faPlusSquare}
+            className="button-add-new-user-gray"
+          />
+        </div>
+        <div class="cercle">
+          {" "}
+          <FontAwesomeIcon
+            icon={faPlusSquare}
+            className="button-add-new-user-gray"
+          />
+        </div>
+        <div class="cercle">
+          {" "}
+          <FontAwesomeIcon
+            icon={faPlusSquare}
+            className="button-add-new-user-gray"
+          />
+        </div>
+      </div>
+    );
   }
 }
 
