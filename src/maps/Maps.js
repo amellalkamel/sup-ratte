6c06b8ab61f54c9b1218dc4970f6c566b1572db0
amellalkamel <amellalkameleddine@gmail.com>
2021-01-01 01:25:07 +0100

    integrate maps

--- src/maps/Maps.js
+++ src/maps/Maps.js
@@ -1,12 +1,29 @@
-import React from "react";
-import "./maps.css";
+import React from 'react';
+import {Map, GoogleApiWrapper} from 'google-maps-react';
+import './maps.css';
+const mapStyles = {
+  width: '100%',
+  height: '80%',
+};
 class Maps extends React.Component {
   constructor(props) {
     super(props);
   }
+
   render() {
-    return <div className="maps">maps</div>;
+    return (
+      <div className="maps">
+        <Map
+          google={this.props.google}
+          zoom={8}
+          style={mapStyles}
+          initialCenter={{lat: 47.444, lng: -122.176}}
+        />
+      </div>
+    );
   }
 }
 
-export default Maps;
+export default GoogleApiWrapper({
+  apiKey: 'TOKEN HERE',
+})(Maps);
